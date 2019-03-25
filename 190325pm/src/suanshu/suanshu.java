e801481f9d5b34839f4524206906b5e6327d8636
zhangzor <2450169510@qq.com>
2019-03-26 07:38:55 +0800

    2019年3月26号

---
+++ 190325pm/src/suanshu/suanshu.java
@@ -0,0 +1,14 @@
+package suanshu;
+
+public class suanshu {
+	public static void main(String[] args) {
+		int a=5,b=2,c;
+		c = a + b;
+		System.out.println("----------------");
+		System.out.println(a + b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println("----------------");
+	}
+}
